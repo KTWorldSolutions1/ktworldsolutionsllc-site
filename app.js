6d74a9f89c1ef10fa68612ee522ebef996504fb5
KTWorldSolutions1 <241440330+KTWorldSolutions1@users.noreply.github.com>
2026-06-14 17:17:31 -0400

    update

--- app.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('.menu-toggle');
-  if (btn) {
-    btn.addEventListener('click', () => {
-      document.body.classList.toggle('menu-open');
-    });
-  }
-});
